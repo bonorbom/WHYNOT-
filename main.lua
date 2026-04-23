9f27b343b372dfd7ba66ce817d040cd30c8453d5
Bon or bom! <bonnallovefly@gmail.com>
2026-04-23 16:37:58 +0700

    Delete main.lua

--- main.lua
+++
@@ -1,6 +0,0 @@
-local base = "https://raw.githubusercontent.com/bonorbom/WHYNOT-/main/"
-
-_G.BON = {}
-
-_G.BON.Fly = loadstring(game:HttpGet(base .. "Fly.lua"))()
-loadstring(game:HttpGet(base .. "HUD.lua"))()
