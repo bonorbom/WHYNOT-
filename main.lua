a718598de593f037864919783aaff9cf0e320d7b
Bon or bom! <bonnallovefly@gmail.com>
2026-04-23 17:39:24 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 _G.BON = {}
 
 _G.BON.Fly = loadstring(game:HttpGet("https://raw.githubusercontent.com/bonorbom/WHYNOT-/main/Fly.lua"))()
+_G.BON.AutoFarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/bonorbom/WHYNOT-/main/autofram.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/bonorbom/WHYNOT-/main/HUD.lua"))()
